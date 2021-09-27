cd81c67baa67f9f0e79a57cc64aeb978adcba366
Totolariflette <62987676+Totolariflette@users.noreply.github.com>
2021-09-27 16:46:20 -0400

    refactors PEP8

--- Agents.py
+++ Agents.py
@@ -1,94 +1,82 @@
 from copy import deepcopy
-from util import legal_moves,count_dust,DIRECTIONS,STAY
-
-class Agent():
-
-    def __init__(self) :
-        self.seq=[]
+from util import legal_moves, count_dust, DIRECTIONS, STAY
 
 
-    def getAction(self,state):
+class Agent:
+
+    def __init__(self):
+        self.seq = []
+
+    def get_action(self, state):
         pass
-        
-        
-        
-class Agent_1(Agent):
 
-    def __init__(self,initial_percept,algo):
-        self.seq=[]
+
+class Agent1(Agent):
+
+    def __init__(self, initial_percept, algo):
+        self.seq = []
         self.update_state(initial_percept)
-        self.goal = lambda state : state["nb_dust"]==0
-        self.init_problem(self.state,self.goal)
+        self.goal = lambda state: state["nb_dust"] == 0
+        self.init_problem(self.state, self.goal)
         self.algo = algo
         self.search()
 
-        
+    def update_state(self, percept):  # = sensor
+        self.state = {'grid': percept.get_grid(), 'pos': percept.agent, 'nb_dust': count_dust(percept.get_grid())}
 
-    def update_state(self,percept): #= sensor
-        self.state={'grid':percept.get_grid(),'pos':percept.agent,'nb_dust':count_dust(percept.get_grid())}
+    def init_problem(self, state, goal):
+        self.problem = Problem1(deepcopy(self.state), goal)
 
-
-    def init_problem(self,state,goal) :
-        self.problem = Problem_1(deepcopy(self.state),goal)
-
-    def search(self) :
+    def search(self):
         self.seq = self.algo(self.problem)
-        #print(self.seq)
-        
+        # print(self.seq)
 
-
-
-    def getAction(self,percept):
-        
-        if len(self.seq) == 0 :
-            self.__init__(percept,self.algo)
-            if len(self.seq) == 0 : self.seq.append(STAY)
+    def get_action(self, percept):
+        if len(self.seq) == 0:
+            self.__init__(percept, self.algo)
+            if len(self.seq) == 0:
+                self.seq.append(STAY)
         print(self.seq)
         action = self.seq.pop(0)
-        
+
         return action
-        
 
 
+class Problem:
 
-
-
-class Problem():
-
-    def is_goal(self,state):
+    def is_goal(self, state):
         pass
 
-    def get_succesor(self,state):
+    def get_succesor(self, state):
         pass
 
-    def get_cost_seq(self,seq):
+    def get_cost_seq(self, seq):
         pass
 
 
-class Problem_1(Problem):
+class Problem1(Problem):
 
-    def __init__(self,state,goal):
+    def __init__(self, state, goal):
         self.initial_state = state
         self.isGoalState = goal
 
-    def getStartState(self):
+    def get_start_state(self):
         return self.initial_state
 
-    def  getSuccessors(self,state):
+    def get_successors(self, state):
         succ = []
-        for act in legal_moves(state["grid"],state["pos"]):
+        for act in legal_moves(state["grid"], state["pos"]):
             n_state = dict()
-            n_state["grid"]=deepcopy(state["grid"])
-            n_state["nb_dust"]=state["nb_dust"]
-            n_state["pos"]=(state["pos"][0],state["pos"][1])
-            if act in DIRECTIONS :
-                n_state["pos"]=(state["pos"][0]+act[0],state["pos"][1]+act[1])
-            else :
+            n_state["grid"] = deepcopy(state["grid"])
+            n_state["nb_dust"] = state["nb_dust"]
+            n_state["pos"] = (state["pos"][0], state["pos"][1])
+            if act in DIRECTIONS:
+                n_state["pos"] = (state["pos"][0] + act[0], state["pos"][1] + act[1])
+            else:
                 if act == "ASPI":
-                    
+
                     n_state["grid"][n_state["pos"][0]][n_state["pos"][1]] = 'p'
-                    if state["grid"][n_state["pos"][0]][n_state["pos"][1]] =='d':
-                        n_state['nb_dust'] -=1
-            succ.append((n_state,act))
+                    if state["grid"][n_state["pos"][0]][n_state["pos"][1]] == 'd':
+                        n_state['nb_dust'] -= 1
+            succ.append((n_state, act))
         return succ
-                    
--- Environment.py
+++ Environment.py
@@ -1,56 +1,54 @@
 from random import randint, random
 from copy import deepcopy
-from util import DIRECTIONS,count_dust    
+from util import DIRECTIONS, count_dust
 
 
-class Environment():
-    def __init__(self,width,height,initial_value='p',p_dirt=0.25,p_jewel=0.01,pos_agt=(0,0)) :
+class Environment:
+    def __init__(self, width, height, initial_value='p', p_dirt=0.25, p_jewel=0.01, pos_agt=(0, 0)):
         self.width = width
         self.height = height
-        self.grid =[[initial_value for _ in range(width) ] for _ in  range(height)]
+        self.grid = [[initial_value for _ in range(width)] for _ in range(height)]
         self.p_jewel = p_jewel
         self.p_dirt = p_dirt
         self.agent = pos_agt
 
     def update(self):
         if random() < self.p_dirt:
-            i=randint(0,self.height-1) 
-            j=randint(0,self.width-1) 
-            if self.grid[i][j] != 'j' : self.grid[i][j] = 'd' 
-            else  : self.grid[i][j] = 'b'
+            i = randint(0, self.height - 1)
+            j = randint(0, self.width - 1)
+            if self.grid[i][j] != 'j':
+                self.grid[i][j] = 'd'
+            else:
+                self.grid[i][j] = 'b'
         if random() < self.p_jewel:
-            i= randint(0,self.width-1) 
-            j=randint(0,self.height-1) 
-            if self.grid[i][j] != 'd' : self.grid[i][j] = 'j' 
-            else  : self.grid[i][j] = 'b'
+            i = randint(0, self.width - 1)
+            j = randint(0, self.height - 1)
+            if self.grid[i][j] != 'd':
+                self.grid[i][j] = 'j'
+            else:
+                self.grid[i][j] = 'b'
 
+    def agent_update(self, action):  # = effector
+        if action in DIRECTIONS:
+            self.agent = (self.agent[0] + action[0], self.agent[1] + action[1])
 
-    def agent_update(self,action): # = effector 
-        if action in DIRECTIONS :
-            self.agent = (self.agent[0]+action[0],self.agent[1]+action[1])
-            
         if action == 'ASPI':
-            self.grid[self.agent[0]][self.agent[1]]='p'
+            self.grid[self.agent[0]][self.agent[1]] = 'p'
 
-        
-
-    def show(self,msg=''):
+    def show(self, msg=''):
         for j in range(self.height):
             for i in range(self.width):
-                #print(i,j,end="|")
-                if self.grid[i][j] =='p' : p =' '
-                if self.grid[i][j] =='d' : p = '#'
-                if self.grid[i][j] =='j' : p = '*'
-                if self.grid[i][j] =='b' : p = '@'
-                if j== self.agent[1] and i == self.agent[0] : p = '>'
-                print('|'+p,end='')
-            if j==1 : print("|   "+msg+"   " + str(self.agent),end="")
-            if j==3 : print("|   "+str(count_dust(self.grid)),end="")
-            
+                # print(i,j,end="|")
+                if self.grid[i][j] == 'p': p = ' '
+                if self.grid[i][j] == 'd': p = '#'
+                if self.grid[i][j] == 'j': p = '*'
+                if self.grid[i][j] == 'b': p = '@'
+                if j == self.agent[1] and i == self.agent[0]: p = '>'
+                print('|' + p, end='')
+            if j == 1: print("|   " + msg + "   " + str(self.agent), end="")
+            if j == 3: print("|   " + str(count_dust(self.grid)), end="")
 
             print("|")
-        
 
-        
     def get_grid(self):
         return deepcopy(self.grid.copy())
