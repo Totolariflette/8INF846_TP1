759de17e17924faa7e1b35050a6a93bdcb5c4600
Louis M <louis.monnier1@uqac.ca>
2021-09-29 09:58:30 -0400

    simplifications ?

--- Agents.py
+++ Agents.py
@@ -1,109 +1,58 @@
 from copy import deepcopy
-from util import legal_moves,count_dust,get_dirty_room, DIRECTIONS,STAY
+from util import legal_moves,count_dust,get_dirty_room, legal_moves_dim, DIRECTIONS,STAY
 
 
-class Agent:
+class Agent_but:
 
-    def __init__(self):
-        self.seq = []
+    def __init__(self,initial_percept,algo,goal):
+        self.seq = [] # sequence d'action prévus par l'agent
+        self.sensor(initial_percept) # fonction prenant une copie de l'environnement en entrée et retournant l'etat interne de l'agent (input :env  output state)
+        self.goal = goal # l'état qui correspond au but 
+        
+        self.algo = algo # alogorithme de recherche dans un arbre (input : problem : output : seq)
+       
+        self.search()
+    
+    def is_goal(self,state):
+        return all( state[k]==self.goal[k] for k in self.goal.keys)
 
-    def get_action(self, state):
+    def sensor(self, percept): 
+        self.bstate = {'grid': percept.get_grid(), 'pos': percept.agent}
+
+  
+
+    def get_succesor(self,state):
         pass
+    
+    def search(self) :
+        self.seq = self.algo(self.bstate,self.is_goal,self.get_succesor)
+        #print(self.seq)
+        
+
+    def get_action(self):
+        
+        while len(self.seq) > 0:
+            action = self.seq.pop(0)
+            print(self.seq)
+            print(self.state)
+            return action
+        
+        self.search()
+        return STAY
 
 
-class Agent1(Agent):
+
+class Agent1(Agent_but):
 
     def __init__(self, initial_percept,algo):
-        self.seq = []
-       
-        self.update_state(initial_percept)
-        self.goal = lambda state : (state["nb_dust"]==0 and state["jewel_gobbed"]==0)
-        self.init_problem()
-        self.algo = algo
-        self.search()
-
-    def update_state(self, percept):  # = sensor
-        self.state = {'grid': percept.get_grid(), 'pos': percept.agent, 'nb_dust': count_dust(percept.get_grid()),'jewel_gobbed':0}
-
-    def init_problem(self) :
-        self.problem = Problem_blind(deepcopy(self.state),self.goal)
-
-    def search(self) :
-        self.seq = self.algo(self.problem)
-        #print(self.seq)
+        super().__init__(initial_percept,algo,goal={'nb_dust':0 ,'jewel_gobbed':0})
+    
         
 
+    def is_goal(self,state):
+        return all([ state[k]==self.goal[k] for k in self.goal.keys()])
 
-    def get_action(self,percept):
-        
-        if len(self.seq) == 0 :
-            self.__init__(percept,self.algo)
-            if len(self.seq) == 0 : self.seq.append(STAY)
-        print(self.seq)
-        print(self.state)
-        action = self.seq.pop(0)
-
-        return action
-
-
-
-class Agent2(Agent):
-
-    def __init__(self, initial_percept,algo,heuristic):
-        self.seq = []
-        self.update_state(initial_percept)
-        self.goal = lambda state : (state["nb_dust"]==0 and state["jewel_gobbed"]==0)
-        self.init_problem()
-        self.algo = algo
-        self.heuristic = heuristic
-        self.search()
-
-    def update_state(self, percept):  # = sensor
-        self.state = {'grid': percept.get_grid(), 'pos': percept.agent, 'nb_dust': count_dust(percept.get_grid()),'jewel_gobbed':0}
-
-    def init_problem(self) :
-        self.problem = Problem_informed(deepcopy(self.state),self.goal)
-
-    def search(self) :
-        self.seq = self.algo(self.problem,self.heuristic)
-        #print(self.seq)
-        
-
-
-    def get_action(self,percept):
-        
-        if len(self.seq) == 0 :
-            self.__init__(percept,self.algo)
-            if len(self.seq) == 0 : self.seq.append(STAY)
-        print(self.seq)
-        print(self.state)
-        action = self.seq.pop(0)
-
-        return action
-
-
-class Problem:
-
-    def is_goal(self, state):
-        pass
-
-    def get_succesor(self, state):
-        pass
-
-    def get_cost_seq(self, seq):
-        pass
-
-
-class Problem_blind(Problem):
-
-    def __init__(self, state, goal):
-        self.initial_state = state
-        self.isGoalState = goal
-
-    def get_start_state(self):
-        return self.initial_state
-
-    def get_successors(self, state):
+    def get_succesor(self,state):
         succ = []
         for act in legal_moves(state["grid"], state["pos"]):
             n_state = dict()
@@ -131,39 +80,32 @@ class Problem_blind(Problem):
                 
             succ.append((n_state, act))
         return succ
-                    
+
+    def sensor(self, percept): 
+        self.bstate = {'grid': percept.get_grid(), 'pos': percept.agent, 'nb_dust': count_dust(percept.get_grid()),'jewel_gobbed':0}
+
+  
 
 
+    def get_action(self):
 
-
-class Problem_informed(Problem):
-    def __init__(self,state,goal):
-        self.initial_state = state
-        self.isGoalState = goal
-
-    def get_start_state(self):
-        return self.initial_state
-
-    def  get_successors(self,state):
-        succ = []
-        for act in legal_moves(state["grid"],state["pos"]):
-            n_state = dict()
-            n_state["grid"]=deepcopy(state["grid"])
-            n_state["dirty_room"]=deepcopy(state["dirty_room"])
-            n_state["pos"]=(state["pos"][0],state["pos"][1])
-            cost = 1
-            if act == STAY : act = 0
-            if act in DIRECTIONS :
-                n_state["pos"]=(state["pos"][0]+act[0],state["pos"][1]+act[1])
-            else :
-                if act == "ASPI":
-                    
-                    n_state["grid"][n_state["pos"][0]][n_state["pos"][1]] = 'p'
-                    if state["grid"][n_state["pos"][0]][n_state["pos"][1]] =='d':
-                        n_state["dirty_room"].remove(n_state["pos"])
-            succ.append((n_state,act,cost))
-        return succ
-
+        if self.bstate["nb_dust"]<=3:
+            self.search()
+        if self.bstate["nb_dust"]>5 :
+            posx,posy= self.bstate["pos"]
+            self.bstate["grid"] = self.bstate["grid"][posx-1:posx+1][posy-1:posy+1]
+            self.bstate["nb_dust"] = count_dust(self.bstate["grid"])
 
         
+        while len(self.seq) > 0:
+            action = self.seq.pop(0)
+            print(self.seq)
+            #print(self.state)
+            return action
+        
+        self.search()
+        return STAY
+        
+        
+
 
--- Game.py
+++ Game.py
@@ -19,7 +19,8 @@ def env_loop(env):
 
 def agent_loop(agent, env):
     while True:
-        action = agent.get_action(env)
+        agent.sensor(env)
+        action = agent.get_action()
         env.agent_update(action)
         sleep(1)
 
@@ -28,16 +29,17 @@ if __name__ == "__main__":
     env = Environment(5, 5, p_dust=0.25, p_jewel=0.05)
     aspi = Agent1(env, breadth_first_search)
 
-    # x = threading.Thread(target=env_loop, args=(env,))
-    # x.start()
-    # y = threading.Thread(target=agent_loop, args=(aspi, env))
-    # y.start()
+    x = threading.Thread(target=env_loop, args=(env,))
+    x.start()
+    y = threading.Thread(target=agent_loop, args=(aspi, env))
+    y.start()
 
-    while True:
-        clear()
-        env.update()
-        action = aspi.get_action(env)
-        env.agent_update(action)
-        env.show()
-        sleep(1)
+    # while True:
+    #     clear()
+    #     env.update()
+    #     aspi.sensor(env)
+    #     action = aspi.get_action()
+    #     env.agent_update(action)
+    #     env.show()
+    #     sleep(1)
 
